d0e86a7ecbc1ad746fec6d04bd291377c4c8e8f9
Cyjuga <fynn-janek.hartmann@edu.bib.de>
2019-12-02 12:59:26 +0100

    UI
    UI

---
+++ train/Assets/Scipts/UI/SwitchPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SwitchPlayer : MonoBehaviour
+{
+    public GameObject playerMenu;
+    public GameObject hotbarPanel;
+    bool playerMenuIsActive = false;
+    void Update()
+    {
+        PlayerMenuActivation();
+    }
+    void PlayerMenuActivation()
+    {
+        if (!playerMenuIsActive)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftAlt))
+            {
+                hotbarPanel.SetActive(false);
+                playerMenu.SetActive(true);
+                playerMenuIsActive = true;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.LeftAlt))
+            {
+                hotbarPanel.SetActive(true);
+                playerMenu.SetActive(false);
+                playerMenuIsActive = false;
+            }
+        }
+    }
+}
