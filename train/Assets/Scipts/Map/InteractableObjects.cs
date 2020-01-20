63a14c11e7d4daaa8877d430ec9f78e3243a5487
niemand <Malte.Fassbender@edu.bib.de>
2020-01-20 11:57:42 +0100

    made good interaction typr

--- train/Assets/Scipts/Map/InteractableObjects.cs
+++ train/Assets/Scipts/Map/InteractableObjects.cs
@@ -10,13 +10,33 @@ public class InteractableObjects : MonoBehaviour
     GameObject text;
     public Text dialog;
     string pressE = "Press E";
-    
+
+    bool isObject = false;
+    bool isDoor = false;
+    bool notFound = false;
+
 
     void Start()
     {
         text = GameObject.Find("Dialog");
+        GetObjectkind();
     }
 
+    void GetObjectkind()
+    {
+        if (this.gameObject.name.Contains("door"))
+        {
+            isDoor = true;
+        }
+        if (this.gameObject.name.Contains("Object"))
+        {
+            isObject = true;
+        }
+        else
+        {
+            notFound = true;
+        }
+    }
 
 
     bool IsNearPlayer()
@@ -28,38 +48,42 @@ public class InteractableObjects : MonoBehaviour
 
                 if (item.GetComponent<Tile>().isOccupied)
                 {
-                     if (this.gameObject == item.GetComponent<Tile>().occupiedObject)
-                     {
-                         return true; 
-                     }
+                    if (this.gameObject == item.GetComponent<Tile>().occupiedObject)
+                    {
+                        return true;
+                    }
 
                 }
             }
         }
-        return false;  
+        return false;
     }
 
     void OnMouseOver()
     {
-        if (!PlayerLogic.isMoving)
+        if (isObject)
         {
 
-            this.gameObject.GetComponent<Renderer>().material.color = Color.green;
-        }
-        if (IsNearPlayer())
-        {
-            text.SetActive(true);
-            Vector3 textPossiton = this.gameObject.transform.position;
-            textPossiton.y = textPossiton.y + 1;
-            dialog.transform.position = textPossiton;
-            dialog.text = pressE;
-            if (Input.GetKey(KeyCode.E)) 
+            if (!PlayerLogic.isMoving)
             {
-                Global.interacted = true;
-                Starting();
+
+                this.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            }
+            if (IsNearPlayer())
+            {
+                text.SetActive(true);
+                Vector3 textPossiton = this.gameObject.transform.position;
+                textPossiton.y = textPossiton.y + 1;
+                dialog.transform.position = textPossiton;
+                dialog.text = pressE;
+                if (Input.GetKey(KeyCode.E))
+                {
+                    Global.interacted = true;
+                    Starting();
+                }
             }
         }
-        
+
     }
     void Starting()
     {
@@ -68,41 +92,29 @@ public class InteractableObjects : MonoBehaviour
 
     void OnMouseExit()
     {
-        this.gameObject.GetComponent<Renderer>().material.color = Color.white;
-        text.SetActive(false);
+        if (isObject)
+        {
+            this.gameObject.GetComponent<Renderer>().material.color = Color.white;
+            text.SetActive(false);
+        }
     }
 
     void GetCurrentTile()
     {
-        Ray ray = new Ray(this.transform.position, Vector3.down);
-        RaycastHit hit;
-        int raylenght = 1;
-        if (Physics.Raycast(ray, out hit, raylenght))
+        if (isObject)
         {
-            if (hit.transform.gameObject.name.Contains("Tile"))
+            Ray ray = new Ray(this.transform.position, Vector3.down);
+            RaycastHit hit;
+            int raylenght = 1;
+            if (Physics.Raycast(ray, out hit, raylenght))
             {
-                hit.transform.gameObject.GetComponent<Tile>().occupiedObject = this.gameObject;
-                hit.transform.gameObject.GetComponent<Tile>().isOccupied = true;
+                if (hit.transform.gameObject.name.Contains("Tile"))
+                {
+                    hit.transform.gameObject.GetComponent<Tile>().occupiedObject = this.gameObject;
+                    hit.transform.gameObject.GetComponent<Tile>().isOccupied = true;
+                }
             }
+            Debug.DrawRay(ray.origin, ray.direction, Color.green);
         }
-        Debug.DrawRay(ray.origin, ray.direction, Color.green);
     }
-
-    //void GetInteractionType()
-    //{
-    //    bool isType = false;
-    //    int i = 0;
-    //    foreach (var item in Global.interactionTypeList)
-    //    {
-    //        //if (this.name = item[i,i])
-    //        //{
-    //        //    isType = true;
-    //        //}
-    //        i++;
-    //    }
-    //    if (!isType)
-    //    {
-    //        Debug.Log("not an interaction type");
-    //    }
-    //}
 }
